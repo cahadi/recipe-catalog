8ffeb71786ae5dbf0026d8a59a0956d7c44159a2
admin <ap357775462534@hmail.com>
2026-04-22 22:58:02 -0400

    feat(routes): add Admin categories routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::prefix('my-recipes')->name('my-recipes.')->middleware('can:be-author')->group(function () {
+        Route::resource('recipes', AuthorRecipeController::class);
+        Route::get('dashboard', [AuthorDashboardController::class, 'index'])->name('dashboard');
+    });
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+       Route::resource('categories', AdminCategoryController::class);
+    });
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
